5abe3fb666bb67c1a36a2e79fea4dfa7165050ca
Saqib Ahmad <you@example.com>
2022-09-03 09:58:59 +0600

    category issues resolved

--- js/script.js
+++ js/script.js
@@ -5,13 +5,23 @@ const loadCategoryTitle = () => {
         .catch(error => console.log('There is a ' + error))
 }
 const displayCategoryTitle = (datas) => {
-    console.log(datas)
+    // console.log(datas)
     const categoryTitleContainer = document.getElementById('category-title');
     datas.forEach(data => {
-        const li = document.createElement('li');
-        li.classList.add('md:p-4')
-        li.innerText =`${data.category_name}`
-        categoryTitleContainer.appendChild(li)
+        // console.log(data.category_id)
+        const span = document.createElement('span');
+        span.innerHTML = `
+        <button onclick="loadNews(${data.category_id})" class="m-2 p-2 md:m-4 bg-red-200">${data.category_name}</button>
+        `
+        categoryTitleContainer.appendChild(span)
     });
+}
+
+const loadNews = (id) => {
+    console.log(id)
+    fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log('There is a ' + error))
 }
 loadCategoryTitle()
